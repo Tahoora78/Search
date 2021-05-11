43415c37586cf0744a05394036735fbe738a1a5b
baharKaviani <bhr.kaviani@gmail.com>
2021-05-12 01:20:55 +0430

    add: role to Squares

--- src/A_star.py
+++ src/A_star.py
@@ -17,20 +17,20 @@ class Table:
         print("row", self.row, "col", self.col)
         for j in range(self.row):
             for i in range(self.col):
-                print("({},{}):".format(i, j), self.table[j][i].cost, end = " ")
+                print("({},{}):".format(i, j), self.table[j][i].cost, self.table[j][i].role, end = " ")
             print()
 
+    """
+    find and set initial state
+    """
     def setR(self, initial_states):
-        x = 0
-        y = 0
         for j in range(self.row):
             for i in range(self.col):
-                if 'r' in initial_states[j][i]:
+                if (self.table[j][i].role == 'r'):
                     x = i
                     y = j
-                    print("i, j", i, j)
-        print("x", x, "y", y, "initial_state", initial_states[y][x])
-        #initial_node = Ids.Node(initial_states, x, y, 0, 'N', int(initial_states[x][y][0]))
+        print("x", x, "y", y, "->", self.table[y][x].cost, self.table[y][x].role)
+        self.r = self.table[y][x]
 
     """
     Create the table with input values
@@ -47,14 +47,19 @@ class Table:
             for i in range(self.col):
                 # make each square
                 if 'x' not in initial_states[j][i]:
-                    newSquare = Square(i, j, int(initial_states[j][i][0]))
-                else:
-                    newSquare = Square(i, j, 1000000)
+                    if 'r' in initial_states[j][i]:   # robot role
+                        newSquare = Square(i, j, int(initial_states[j][i][0]), 'r')
+                    elif 'b' in initial_states[j][i]: # butter role
+                        newSquare = Square(i, j, int(initial_states[j][i][0]), 'b')
+                    elif 'p' in initial_states[j][i]: # goal role
+                        newSquare = Square(i, j, int(initial_states[j][i][0]), 'p')
+                    else:                             # normal role
+                        newSquare = Square(i, j, int(initial_states[j][i][0]), 'n')
+                else:                                 # x role
+                    newSquare = Square(i, j, 1000000, 'x')
 
                 col.append(newSquare)
             self.table.append(col)
-
-        print(initial_states)
 
         # print the table to check
         Table.printTable(self)
@@ -66,10 +71,11 @@ class Table:
 
 
 class Square:
-    def __init__(self, x, y, cost):
+    def __init__(self, x, y, cost, role):
         self.x = x
         self.y = y
         self.cost = cost
+        self.role = role
         self.heuristicCost = 0
         self.arrayCost = []
 
